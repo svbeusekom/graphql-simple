c48bf9313da0814d0cceab14eeddbbb09ff71fd8
Stefan van Beusekom <stefan.van.beusekom@amsterdam.nl>
2023-08-23 13:07:24 +0200

    Created a small GraphQL service example to illustrate how simplistic it is to setup.

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ server.start().then(() => {
     server.applyMiddleware({ app });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}${server.graphqlPath}`);
